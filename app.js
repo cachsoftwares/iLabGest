8fb4a3f81409fed0d3995ac7d8532ed38948ecb6
cachsoftwares <cachsoftwares@gmail.com>
2026-06-05 23:00:32 +0100

    Server Fly

---
+++ app.js
@@ -0,0 +1,8 @@
+/* modules import */
+import express from 'express'
+
+/* app config */
+const app = express()
+
+/* app listen */
+app.listen(3025, () => console.log(`Server Fly - http://localhost:3025`))
