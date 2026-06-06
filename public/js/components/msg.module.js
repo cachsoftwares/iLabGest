a61cb35a693c36eb35732eb091e48504ba3b38c5
cachsoftwares <cachsoftwares@gmail.com>
2026-06-06 22:06:32 +0100

    flash msg

---
+++ public/js/components/msg.module.js
@@ -0,0 +1,13 @@
+document.body.onload = () => {
+    if(document.getElementById("snackbar")) {
+
+        // Get the snackbar DIV
+        var snackbar = document.getElementById("snackbar")
+        
+        // Add the "show" class to DIV
+        snackbar.className = "show";
+        
+        // After 3 seconds, remove the show class from DIV
+        setTimeout(function(){ snackbar.className = snackbar.className.replace("show", ""); }, 3000);
+    }
+}
