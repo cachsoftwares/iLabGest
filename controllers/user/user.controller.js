42573ecce90bffb41f709d9f061498c5d14eed6b
cachsoftwares <cachsoftwares@gmail.com>
2026-06-06 22:50:59 +0100

    sign in & out modifing user status

--- controllers/user/user.controller.js
+++ controllers/user/user.controller.js
@@ -1,6 +1,9 @@
 /* modules import */
 import passport from 'passport'
 
+/* models import */
+import User from '../../models/user.model.js'
+
 /* controllers */
 export const sign = (req, res) => {
 
@@ -18,23 +21,60 @@ export const signin = (req, res, next) => {
 
 export const signout = (req, res) => {
 
+    const userId = req.user._id
+
     req.logout(err => {
 
-        if(err) {
+        if (err) {
 
             console.log(`Erro Interno: ${err}`)
             req.flash('error_msg', 'Erro Interno')
             res.redirect('/user/profile')
-        
+
         } else {
 
-            req.flash('info_msg', 'Conta Fechada')
-            res.redirect('/user/sign')
+            User.findByIdAndUpdate(
+                { _id: userId },
+                { $set: { status: 'Offline' } }
+            ).then(() => {
+
+                req.flash('info_msg', 'Conta Fechada')
+                res.redirect('/user/sign')
+
+            }).catch(err => {
+
+                console.log(`Erro Interno: ${err}`)
+                req.flash('error_msg', 'Erro Interno')
+                res.redirect('/user/sign')
+            })
         }
     })
 }
 
-export const profile = (req, res) => {
+export const profile = async (req, res) => {
+
+    const user = await User.findOne({ _id: req.user._id })
+
+    if (user.status != 'Online') {
+
+        await User.findByIdAndUpdate(
+
+            { _id: req.user._id },
+            { $set: { status: 'Online' } }
+
+        ).then(() => {
+
+            res.render('user/profile')
+            return
+
+        }).catch(err => {
+
+            console.log(`Erro Interno: ${err}`)
+            req.flash('error_msg', 'Erro Interno')
+            res.redirect('/')
+            return
+        })
+    }
 
     res.render('user/profile')
 }
