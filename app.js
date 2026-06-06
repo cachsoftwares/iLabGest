6212a3ad363812dcd82c80fb21f63054259717eb
cachsoftwares <cachsoftwares@gmail.com>
2026-06-06 14:50:10 +0100

    handlebars config

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 /* modules import */
 import express from 'express'
 import dotenv from 'dotenv'
+import handlebars from 'express-handlebars'
 
 /* app config */
 const app = express()
 
 /* dotenv config */
 dotenv.config()
+
+/* handlebars config */
+const hdb = handlebars.create({
+    defaultLayout: 'main',
+    extname: '.hbs'
+})
+
+app.engine('hdb', hdb.engine)
+app.set('view engine', 'hdb')
 
 /* app listen */
 app.listen(process.env.PORT, () => console.log(`Server Fly - http://localhost:${process.env.PORT}`))
