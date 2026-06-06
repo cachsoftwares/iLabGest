5a24fced03feb0f89b68e281b57ae18aa9583108
cachsoftwares <cachsoftwares@gmail.com>
2026-06-06 21:04:22 +0100

    createAdminIfNotExist

--- helpers/createAdmin.help.js
+++ helpers/createAdmin.help.js
@@ -6,31 +6,39 @@ import User from '../models/user.model.js'
 
 export default async function createAdminIfNotExist() {
 
-    /* get data */
-    const data = {
-        name: 'Administrador do Sistema',
-        tel: '923100100',
-        pwd: process.env.ADMIN_PWD,
-        role: 'Administrador'
-    }
+    /* find an admin */
+    User.findOne({ role: 'Administrador' }).then(async (admin) => {
 
-    /* crypto pwd */
-    await bcrypt.hash(data.pwd, 10, async (err, hash) => {
+        if (!admin) {
 
-        if (err) {
-            
-            console.log(`Erro Interno: ${err}`)
-            return
+            /* get data */
+            const data = {
+                name: 'Administrador do Sistema',
+                tel: '923100100',
+                pwd: process.env.ADMIN_PWD,
+                role: 'Administrador'
+            }
+
+            /* crypto pwd */
+            await bcrypt.hash(data.pwd, 10, async (err, hash) => {
+
+                if (err) {
+
+                    console.log(`Erro Interno: ${err}`)
+                    return
+                }
+
+                /* replace pwd for hash */
+                data.pwd = hash
+
+                /* save into db */
+                await new User(data).save().then(() => {
+
+                    console.log('Admin created')
+
+                }).catch(err => console.log(`Erro Interno: ${err}`))
+            })
         }
 
-        data.pwd = hash
-
-        /* save into db */
-        await new User(data).save().then(() => {
-
-            console.log('Admin created')
-
-        }).catch(err => console.log(`Erro Interno: ${err}`))
-    })
-
+    }).catch(err => console.log(`Erro Interno: ${err}`))
 }
